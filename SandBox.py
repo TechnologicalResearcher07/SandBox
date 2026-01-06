1e0912961aeaaf11dc37b1673d3ffbae3b0af069
TechnologicalResearcher07 <investedcomputerexplorer07@gmail.com>
2026-01-06 14:11:56 -0500

    Added variables with User Input

--- SandBox.py
+++ SandBox.py
@@ -9,3 +9,11 @@ name = "Kiera McKimmy"
 age = 15
 
 print("My name is",name,". It is the year",year,", and I am",age,"years old!")
+
+print("")
+
+year = input("What year is it: ")
+name = input("What is your name: ")
+age = input("How old are you: ")
+
+print("\nWelcome",name,"I see you are",age,"years old in the year of",year + ".")
